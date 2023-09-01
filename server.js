caf218fb94ca3b9a3a2033bbfec1ca3c92877840
Gustavo <gustavoneripeixoto@gmail.com>
2023-09-01 14:25:51 -0700

    GET data added

--- server.js
+++ server.js
@@ -7,3 +7,21 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const data = [];
+
+// GET all items
+app.get('/items', (req, res) => {
+  res.json(data);
+});
+
+// GET a specific item by ID
+app.get('/items/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = data.find((item) => item.id === id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
+});
