1be93e5fe38d7d1bd6960c95b63abe405ce94c9f
Gustavo <gustavoneripeixoto@gmail.com>
2023-09-01 15:10:50 -0700

    UPdate an existing item by ID

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/items', (req, res) => {
   res.json(data);
 });
 
-// GET a specific item by ID
+// GET specific item by ID
 app.get('/items/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const item = data.find((item) => item.id === id);
@@ -31,4 +31,19 @@ app.post('/items', (req, res) => {
     const newItem = req.body;
     data.push(newItem);
     res.status(201).json(newItem);
+  });
+
+
+// PUT (update) an existing item by ID
+app.put('/items/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedItem = req.body;
+    const index = data.findIndex((item) => item.id === id);
+  
+    if (index !== -1) {
+      data[index] = updatedItem;
+      res.json(updatedItem);
+    } else {
+      res.status(404).json({ message: 'Item not found' });
+    }
   });
